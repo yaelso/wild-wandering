61570c9c246bac943b76c9083be02f163e6ab48d
Yael <yaelasolomon@gmail.com>
2023-06-09 12:00:52 -0700

    Update naming conventions

--- api.py
+++ api.py
@@ -2,5 +2,5 @@ from pyrule_compendium import compendium
 
 comp = compendium()
 
-def get_random_entry(number):
+def get_numbered_entry(number):
     return print(comp.get_entry(number))
--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import api
 
-api.get_random_entry(input("Enter a number to view an entry: "))
+api.get_numbered_entry(input("Enter a number to view an entry: "))
